b0e976c8c49b2d933c264a5ab4528c2d380a2b2b
ShahjamalMalik <shahjamalmalik@gmail.com>
2020-04-24 22:11:07 +0000

    Added in countOnly function, counts only the values given in the object

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for(let key in itemsToCount) {
+    if(itemsToCount[key] === true) {
+      results[key] = 0
+      for(let item of allItems) {
+        if (key === item) {
+          results[key] += 1
+        }
+      }
+    }
+  }
+
+
+
+  return results; 
+}
+
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+console.log(countOnly([,"b","c","d","e","f","g","h","j", "a", "i"], {"a": true, "b":true, "c":false, "d":true, "e":false, "f":true, "g":false, "h":false, "k":true, "l":true, "i":true, "z": false}))
+
+
+console.log({1: true, 2:true, 3:false, 4:true, 5:false})
